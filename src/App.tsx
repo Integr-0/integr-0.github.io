cf401ecf61af6f8ce27ee0ac485ea7c30701208d
Integr-0 <erik.reitbauer@outlook.de>
2023-12-01 19:08:40 +0100

    Add project files.

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+function App() {
+  return (
+      <>
+          <div style={{ alignItems: 'center', display: 'flex', marginLeft: "auto", marginRight: "auto", marginTop: "5%", justifyContent: "space-evenly" }}>
+              <div style={{ width: "467px", height: "339", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
+                  <img style={{ width: 233, height: 233, marginLeft: "10px", marginRight: "10px", marginTop: "10px", marginBottom: "10px", borderRadius: "120px", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} src="src/assets/logo.png" />
+
+                  <div style={{ width: 334, height: 233, marginTop: "120px", padding: "10px" }}>
+                      <div style={{ color: '#6D5A5A', fontSize: 40, fontFamily: 'Arial', fontWeight: '700', wordWrap: 'break-word', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Erik</div>
+                      <div style={{ color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', fontWeight: '300', wordWrap: 'break-word', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Integr</div>
+                  </div>
+              </div>
+
+              <div style={{ width: "213px", height: "375", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
+                  <div style={{ width: 213, height: 375, position: 'relative' }}>
+                      <div style={{ width: 111, height: 28, left: 51, top: 276, position: 'absolute' }}>
+                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src="src/assets/link.png" />
+
+                          <a style={{ left: 35, top: 2, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '700', textDecoration: 'underline', wordWrap: 'break-word' }} href="https://discordapp.com/users/688059979105697844">Discord</a>
+                      </div>
+
+                      <div style={{ width: 102, height: 28, left: 51, top: 231, position: 'absolute' }}>
+                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src="src/assets/link.png" />
+
+                          <a style={{ left: 35, top: 2, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '700', textDecoration: 'underline', wordWrap: 'break-word' }} href="https://github.com/Integr-0">Github</a>
+                      </div>
+
+                      <div style={{ width: 126, height: 48, left: 51, top: 33, position: 'absolute' }}>
+                          <img style={{ width: 28.90, height: 33.03, left: 0, top: 8, position: 'absolute' }} src="src/assets/newBook.png" />
+
+                          <div style={{ left: 35, top: 0, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '600', wordWrap: 'break-word' }}>HTBLA<br />Leonding</div>
+                      </div>
+                  </div>
+              </div>
+
+              
+          </div>
+          <div style={{ alignItems: 'center', display: 'flex', marginLeft: "auto", marginRight: "auto", marginTop: "5%", justifyContent: "space-evenly" }}>
+              <div style={{ width: "701px", height: "107", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
+                
+                  <div style={{ width: 701, height: 107 }}>
+                      <img style={{ width: 50, height: 50, left: 47, top: 29, position: 'absolute' }} src="src/assets/kotlin.png" />
+                      <img style={{ width: 50, height: 50, left: 117, top: 29, position: 'absolute' }} src="src/assets/java.png" />
+                      <img style={{ width: 50, height: 50, left: 187, top: 29, position: 'absolute' }} src="src/assets/csharp.png" />
+                      <img style={{ width: 50, height: 50, left: 468, top: 29, position: 'absolute' }} src="src/assets/python.png" />
+                      <img style={{ width: 50, height: 50, left: 257, top: 29, position: 'absolute' }} src="src/assets/css.png" />
+                      <img style={{ width: 50, height: 50, left: 398, top: 29, position: 'absolute' }} src="src/assets/html.png" />
+                      <img style={{ width: 50, height: 50, left: 328, top: 29, position: 'absolute' }} src="src/assets/react.png" />
+                      <img style={{ width: 50, height: 50, left: 538, top: 29, position: 'absolute' }} src="src/assets/ts.png" />
+                      <img style={{ width: 50, height: 50, left: 608, top: 29, position: 'absolute' }} src="src/assets/js.png" />
+
+                  </div>
+                  
+              </div>
+              
+              
+             
+          </div>
+          
+      </>
+
+
+  )
+}
+
+export default App
