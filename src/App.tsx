27eca5c3f826b454303436c875f69cca9e2f4798
Integr-0 <erik.reitbauer@outlook.de>
2023-12-01 19:36:26 +0100

    Update 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import './App.css'
-//import cSharpImg from './assets/csharp.png'
-//import htmlImg from './assets/html.png'
-//import javaImg from './assets/java.png'
-//import jsImg from './assets/js.png'
-//import kotlinImg from './assets/kotlin.png'
-//import linkImg from './assets/link.png'
-//import logoImg from './assets/logo.png'
-//import bookImg from './assets/newBook.png'
-//import pythonImg from './assets/python.png'
-//import reactImg from './assets/react.png'
-//import tsImg from './assets/ts.png'
+import cSharpImg from './assets/csharp.png'
+import cssImg from './assets/css.png'
+import htmlImg from './assets/html.png'
+import javaImg from './assets/java.png'
+import jsImg from './assets/js.png'
+import kotlinImg from './assets/kotlin.png'
+import linkImg from './assets/link.png'
+import logoImg from './assets/logo.png'
+import bookImg from './assets/newBook.png'
+import pythonImg from './assets/python.png'
+import reactImg from './assets/react.png'
+import tsImg from './assets/ts.png'
 
 function App() {
   return (
       <>
           <div style={{ alignItems: 'center', display: 'flex', marginLeft: "auto", marginRight: "auto", marginTop: "5%", justifyContent: "space-evenly" }}>
               <div style={{ width: "467px", height: "339", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
-                  <img style={{ width: 233, height: 233, marginLeft: "10px", marginRight: "10px", marginTop: "10px", marginBottom: "10px", borderRadius: "120px", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} src="src/assets/logo.png" />
+                  <img style={{ width: 233, height: 233, marginLeft: "10px", marginRight: "10px", marginTop: "10px", marginBottom: "10px", borderRadius: "120px", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} src={logoImg} />
 
                   <div style={{ width: 334, height: 233, marginTop: "120px", padding: "10px" }}>
                       <div style={{ color: '#6D5A5A', fontSize: 40, fontFamily: 'Arial', fontWeight: '700', wordWrap: 'break-word', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Erik</div>
@@ -27,19 +28,19 @@ function App() {
               <div style={{ width: "213px", height: "375", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
                   <div style={{ width: 213, height: 375, position: 'relative' }}>
                       <div style={{ width: 111, height: 28, left: 51, top: 276, position: 'absolute' }}>
-                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src="src/assets/link.png" />
+                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src={linkImg} />
 
                           <a style={{ left: 35, top: 2, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '700', textDecoration: 'underline', wordWrap: 'break-word' }} href="https://discordapp.com/users/688059979105697844">Discord</a>
                       </div>
 
                       <div style={{ width: 102, height: 28, left: 51, top: 231, position: 'absolute' }}>
-                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src="src/assets/link.png" />
+                          <img style={{ width: 28.09, height: 28, left: 0, top: 0, position: 'absolute' }} src={linkImg} />
 
                           <a style={{ left: 35, top: 2, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '700', textDecoration: 'underline', wordWrap: 'break-word' }} href="https://github.com/Integr-0">Github</a>
                       </div>
 
                       <div style={{ width: 126, height: 48, left: 51, top: 33, position: 'absolute' }}>
-                          <img style={{ width: 28.90, height: 33.03, left: 0, top: 8, position: 'absolute' }} src="src/assets/newBook.png" />
+                          <img style={{ width: 28.90, height: 33.03, left: 0, top: 8, position: 'absolute' }} src={bookImg} />
 
                           <div style={{ left: 35, top: 0, position: 'absolute', color: '#6D5A5A', fontSize: 20, fontFamily: 'Arial', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', fontWeight: '600', wordWrap: 'break-word' }}>HTBLA<br />Leonding</div>
                       </div>
@@ -52,15 +53,15 @@ function App() {
               <div style={{ width: "701px", height: "107", backdropFilter: 'blur(2.5px)', alignItems: 'center', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', display: 'flex', marginLeft: "auto", marginRight: "auto", background: 'rgba(177.44, 177.44, 177.44, 0.75)', borderRadius: 46, marginTop: "5%" }}>
                 
                   <div style={{ width: 701, height: 107 }}>
-                      <img style={{ width: 50, height: 50, left: 47, top: 29, position: 'absolute' }} src="src/assets/kotlin.png" />
-                      <img style={{ width: 50, height: 50, left: 117, top: 29, position: 'absolute' }} src="src/assets/java.png" />
-                      <img style={{ width: 50, height: 50, left: 187, top: 29, position: 'absolute' }} src="src/assets/csharp.png" />
-                      <img style={{ width: 50, height: 50, left: 468, top: 29, position: 'absolute' }} src="src/assets/python.png" />
-                      <img style={{ width: 50, height: 50, left: 257, top: 29, position: 'absolute' }} src="src/assets/css.png" />
-                      <img style={{ width: 50, height: 50, left: 398, top: 29, position: 'absolute' }} src="src/assets/html.png" />
-                      <img style={{ width: 50, height: 50, left: 328, top: 29, position: 'absolute' }} src="src/assets/react.png" />
-                      <img style={{ width: 50, height: 50, left: 538, top: 29, position: 'absolute' }} src="src/assets/ts.png" />
-                      <img style={{ width: 50, height: 50, left: 608, top: 29, position: 'absolute' }} src="src/assets/js.png" />
+                      <img style={{ width: 50, height: 50, left: 47, top: 29, position: 'absolute' }} src={kotlinImg} />
+                      <img style={{ width: 50, height: 50, left: 117, top: 29, position: 'absolute' }} src={javaImg} />
+                      <img style={{ width: 50, height: 50, left: 187, top: 29, position: 'absolute' }} src={cSharpImg} />
+                      <img style={{ width: 50, height: 50, left: 468, top: 29, position: 'absolute' }} src={pythonImg} />
+                      <img style={{ width: 50, height: 50, left: 257, top: 29, position: 'absolute' }} src={cssImg} />
+                      <img style={{ width: 50, height: 50, left: 398, top: 29, position: 'absolute' }} src={htmlImg} />
+                      <img style={{ width: 50, height: 50, left: 328, top: 29, position: 'absolute' }} src={reactImg} />
+                      <img style={{ width: 50, height: 50, left: 538, top: 29, position: 'absolute' }} src={tsImg} />
+                      <img style={{ width: 50, height: 50, left: 608, top: 29, position: 'absolute' }} src={jsImg} />
 
                   </div>
                   
