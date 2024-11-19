b4d84a9e9ad3cb07f45427560d1449d68108cc5e
Integr-0 <erik.reitbauer@outlook.de>
2024-11-19 15:07:53 +0100

    a

--- src/assets/Data.tsx
+++ src/assets/Data.tsx
@@ -3,36 +3,36 @@ export const Repos = [
         name: "Castl3d-Frontend",
         description: "Frontend for the Castl3d chess app.",
         html_url: "https://github.com/Integr-0/Castl3d-Frontend",
-        readme: "assets/Castl3d-Frontend.md"
+        readme: "Castl3d-Frontend.md"
     },
     {
         name: "Castl3d-Backend",
         description: "Backend for the Castl3d chess app.",
         html_url: "https://github.com/Integr-0/Castl3d-Backend",
-        readme: "assets/Castl3d-Backend.md"
+        readme: "Castl3d-Backend.md"
     },
     {
         name: "Helix",
         description: "A utility mod adding various features.",
         html_url: "https://github.com/Integr-0/Helix",
-        readme: "assets/Helix.md"
+        readme: "Helix.md"
     },
     {
         name: "ContentAutomation",
         description: "Automated bot to generate content for various social media platforms",
         html_url: "https://github.com/Integr-0/ContentAutomation",
-        readme: "assets/ContentAutomation.md"
+        readme: "ContentAutomation.md"
     },
     {
         name: "InHttp",
         description: "Simple but effective HTTP Client for Kotlin",
         html_url: "https://github.com/Integr-0/InHttp",
-        readme: "assets/InHttp.md"
+        readme: "InHttp.md"
     },
     {
         name: "RegistryTracker",
         description: "Track entries in JSON format",
         html_url: "https://github.com/Integr-0/RegistryTracker",
-        readme: "assets/RegistryTracker.md",
+        readme: "RegistryTracker.md",
     }
 ]
